bba3bd98cb76cc8ab6828c48b354033cdc032c17
Vaibhav Patil <96064276+VaibhavAPatil@users.noreply.github.com>
2022-12-25 14:28:29 +0530

    Create Sum1to100.c

---
+++ Sum1to100.c
@@ -0,0 +1,13 @@
+// Sum of first 100 Integer/Numbers
+
+#include <stdio.h>
+
+int main() {
+    int sum, i;
+    for(i=1; i<=100; i++){
+       sum = sum + i; 
+    }
+    printf(" Sum of 1 To 100 : %d",sum);
+
+    return 0;
+}
